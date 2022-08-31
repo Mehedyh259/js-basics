b1e3e2b0440b6df00975cc6259146acdbd5f6a95
Mehedyh259 <mehedyh259@gmail.com>
2022-09-01 00:18:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,171 @@
+// var Name = "mehedi hasan"
+// var x = 5;
+// var male = false;
+// var arr = [424, 324, 42342, 2, 3, 43];
+// var person = {
+//     name: 'mehedi',
+//     age: 25,
+//     job: 'bekar'
+// }
+// console.log(isNaN(Name))
+
+// x number noy => false
+// Name number noy => true
+
+// var name = 'mehedy';
+// name = 'hasan';
+// var name = 'rokib vai';
+
+// let naam = 'mehedy';
+// naam = 'hasan'; // possible
+// let naam = 'rokib vai'; // not possible
+
+// const naam = 'mheedy';
+// naam = 'hasan' // not possible
+// const naam = 'hasan' // not possible
+
+// console.log(naam)
+
+
+// const number = 39;
+// const x = 10;
+
+// const rokib = number % x;
+
+// console.log(rokib)
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+
+// const i = 0;
+
+// i = i + 1;
+// i++;
+// console.log(i)
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// const isRaining = false;
+// const naam = 'mfafadf'
+
+// if (naam === 'mehedy') {
+//     console.log('cele')
+// } else {
+//     console.log('meye')
+// }
+
+// const name = 'naam nai';
+// const name1 = "name nai abar"
+// console.log(name)
+// console.log(name1)
+
+// function showName(name) {
+//     console.log(name)
+// }
+// showName("mehedy")
+// showName("hasan")
+// showName(name1)
+
+
+// function results(x, y, z) {
+//     return x + y + z;
+// }
+
+// const rslt = results(1, 2, 3)
+// console.log(rslt)
+
+
+// function feetToInch(feet) {
+//     const inch = feet * 12;
+//     return inch
+// }
+
+// const inchees = feetToInch(5)
+// console.log(inchees);
+
+// function hotel(singara, puri, moglai) {
+//     const singaraPrice = 5;
+//     const puriPrice = 10;
+//     const moglaiPrice = 20;
+
+//     const total = (singara * singaraPrice) + (puri * puriPrice) + (moglai * moglaiPrice)
+//     return total
+
+// const total = hotel(2, 2, 1)
+// console.log(total)
+// }
+
+// function checkFile(fileName) {
+//     return fileName.endsWith('gmail.com')
+// }
+
+// const email = 'abc@gmail.com';
+// const email1 = 'abc@yahoo.com';
+
+// const result = checkFile(email1)
+// console.log(result)
+
+// function checkVai(x, y) {
+//     if (typeof x === 'object' && typeof y === 'object') {
+//         if (x.name === y.brother && x.brother === y.name) {
+//             return true;
+//         } else {
+//             return false
+//         }
+//     } else {
+//         return 'please give two objects as perameters'
+//     }
+// }
+
+
+// const person1 = {
+//     name: 'abul',
+//     brother: 'babul'
+// }
+// const person2 = {
+//     name: 'babul',
+//     brother: 'abul'
+// }
+
+// const result = checkVai(person1, person2)
+// console.log(result);
+
+
+// plane = 100, train = 30; van = per person 5 taka
+
+// function vanVara(peoples) {
+//     if (typeof peoples === 'number') {
+//         // per plane 100 jon
+//         const plane = 100;
+//         const train = 30;
+//         const vara = 5;
+
+//         const remainAfterPlane = peoples % plane;
+//         const remainAfterTrain = remainAfterPlane % train
+
+//         const totalVara = remainAfterTrain * vara;
+//         return totalVara;
+//     } else {
+//         return 'please provide valid number'
+//     }
+// }
+
+// const total = vanVara(232)
+// console.log(total)
+
+
+
+
+
+// const obj = {
+//     name: 'something',
+//     age: 32,
+//     job: 'nai'
+// }
+
+// console.log(obj.job)
